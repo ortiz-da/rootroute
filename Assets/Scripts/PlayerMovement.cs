23307340914c8625a0ad4139560867e8b1591bc8
ortiz-da <ortiz.da@northeastern.edu>
2023-02-03 23:40:35 -0500

    2d movemnt and tiles

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://youtu.be/dwcT-Dch0bA
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal = 0f;
+    private float vertical;
+    public CharacterController controller;
+    private bool jump = false;
+
+    public float runSpeed = 40f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontal = Input.GetAxisRaw("Horizontal") * runSpeed;
+        // vertical = Input.GetAxisRaw("Vertical");
+        Debug.Log(horizontal);
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontal * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
