ac61cf85bccd8ddf3992b1e1004f2b0f557db2fd
Laurel Parsons <34589706+LaurelParsons@users.noreply.github.com>
2023-02-04 11:52:14 -0500

    Added HUD Elements

---
+++ Assets/Scripts/TreeHealth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeHealth : MonoBehaviour
+{
+    public Image healthImage;
+    private TreeManager tree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthImage.fillAmount = Mathf.Clamp(tree.health / tree.maxHealth, 0f, 1f);
+    }
+}
