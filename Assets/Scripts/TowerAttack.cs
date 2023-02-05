0e6280c9bf99b480e4ea21e22a0143723669ec98
Laurel Parsons <34589706+LaurelParsons@users.noreply.github.com>
2023-02-04 22:39:24 -0500

    Added magic tower

--- Assets/Scripts/TowerAttack.cs
+++ Assets/Scripts/TowerAttack.cs
@@ -7,37 +7,51 @@ public class TowerAttack : MonoBehaviour
 {
     [SerializeField] private GameObject beetle;
     public GameObject bullet;
+    private Animator animator;
     [SerializeField] private float distanceToBeetle;
 
-    private bool attacking = false;
+    public GameObject particles;
+
+    private bool isAttacking = false;
 
     // Start is called before the first frame update
     void Start()
     {
         beetle = GameObject.FindGameObjectWithTag("AGEnemy");
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(attacking)
-        {
-
-        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Debug.Log("Hit trigger");
+        isAttacking= true;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        attacking = false;
+        animator.SetBool("attacking", false);
+        StopAllCoroutines();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!isAttacking)
+        {
+            StartCoroutine(attack());
+        }
     }
 
     IEnumerator attack()
     {
+        Instantiate(particles, transform.position, Quaternion.identity);
 
+        yield return new WaitForSeconds(VariableSetup.rate);
+        isAttacking= false;
     }
 }
