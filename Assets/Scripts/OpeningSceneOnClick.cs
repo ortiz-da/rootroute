318148edf5f564da0a09f01ba6f961b452fce0c6
Jason Mark <jason@jasonondesign.com>
2023-02-05 15:13:03 -0500

    Opening Screens work!

---
+++ Assets/Scripts/OpeningSceneOnClick.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningSceneOnClick : MonoBehaviour
+{
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            //Save stuff
+            SceneManager.LoadScene("LoadingScene");
+            Debug.Log("This is a log message.");
+        }
+    }
+}
