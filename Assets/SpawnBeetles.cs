589e897717759fb192c904a53a50dc7fb6763ec0
ortiz-da <ortiz.da@northeastern.edu>
2023-02-06 22:22:03 -0500

    delete worm detector

--- Assets/SpawnBeetles.cs
+++ Assets/SpawnBeetles.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnBeetles : MonoBehaviour
@@ -7,44 +6,42 @@ public class SpawnBeetles : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject beetle;
-
-    private int waveNumber = 0;
-
-    private int maxSpawn = 3;
+    public AudioClip waveStartSound;
 
     private AudioSource audioSource;
-    public AudioClip waveStartSound;
-    void Start()
+
+    private readonly int maxSpawn = 5;
+
+    private int waveNumber;
+
+    private void Start()
     {
         audioSource = GetComponent<AudioSource>();
         StartCoroutine(SpawnBeetle());
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
     }
 
-    IEnumerator SpawnBeetle()
+    private IEnumerator SpawnBeetle()
     {
         yield return new WaitForSeconds(10);
         while (waveNumber < 15)
         {
-            maxSpawn++;
             SpawnWave();
-            
+
             yield return new WaitForSeconds(Random.Range(5, 15));
             waveNumber++;
         }
-
     }
 
-    IEnumerator SpawnWave()
+    private IEnumerator SpawnWave()
     {
         audioSource.clip = waveStartSound;
         audioSource.Play();
-        for(int i = 0; i < Random.Range(3, maxSpawn); i++) 
+        for (var i = 0; i < Random.Range(3, maxSpawn); i++)
         {
             Instantiate(beetle);
             yield return new WaitForSeconds(Random.Range(0f, 2f));
